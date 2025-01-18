7d06edf2b1185a1332c6057694c3a3595bc551d3
ATavares2k <andrecmtavares@gmail.com>
2025-01-18 18:19:47 +0000

    minor updates and starting setup

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>The Cabin Oasis. Wellcome to the Paradise!</h1>
+      <Link href="/cabins">Explore luxury Cabins!</Link>
+    </div>
+  );
+}
