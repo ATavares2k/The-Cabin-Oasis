8832cfe2a2b23b14ab4590806bb9530c7fc3094a
ATavares2k <andrecmtavares@gmail.com>
2025-01-20 15:39:21 +0000

    major changes and layout updates

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -1,5 +1,20 @@
+import SideNavigation from "../_components/SideNavigation";
+
 export const metadata = { title: "Guest area" };
 
 export default function Page() {
-  return <h1>Your Account</h1>;
+  return (
+    <div>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Wellcome User
+      </h2>
+      <p>
+        We&apos;re here to make your stay unique and comfortable. <br /> Whether
+        you&apos;re traveling for work or leisure, we offer carefully prepared
+        accommodations to ensure unforgettable moments.
+      </p>
+      <br />
+      <p>So Please Login so we can get to know you a little Better :) 💛</p>
+    </div>
+  );
 }
