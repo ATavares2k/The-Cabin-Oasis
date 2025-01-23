7ed4f5e8b368fbd06d757b6611651025c584bd0a
ATavares2k <andrecmtavares@gmail.com>
2025-01-23 16:59:51 +0000

    cabins, notfound, global errors and cabin page

--- app/cabins/page.jsx
+++ app/cabins/page.jsx
@@ -1,11 +1,10 @@
-import CabinCard from "@/app/_components/CabinCard";
-import { getCabins } from "@/app/_lib/data-service";
+import CabinList from "../_components/CabinList";
+import SpinnerMini from "../_components/SpinnerMini";
+import { Suspense } from "react";
 
 export const metadata = { title: "Cabins" };
 
-export default async function Page() {
-  const cabins = await getCabins();
-
+export default function Page() {
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -20,13 +19,9 @@ export default async function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<SpinnerMini />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
